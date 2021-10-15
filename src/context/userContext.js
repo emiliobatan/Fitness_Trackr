805c8411cd575124fccd63a0f3df9830e6e8c9bf
Emilio <emiliobatan@ymail.com>
2021-10-15 15:47:31 -0700

    created userContext and a util folder with a request function

---
+++ src/context/userContext.js
@@ -0,0 +1,21 @@
+import { useState, useContext, useEffect } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [token, setToken] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    return <UserContext.Provider
+        value={{
+            user,
+            setUser,
+            token,
+            setToken,
+            loggedIn,
+            setLoggedIn
+        }}>{children}
+    </UserContext.Provider>
+
+}
---
+++ src/util/index.js
@@ -0,0 +1,24 @@
+const { REACT_APP_API_URL = 'https://fitnesstrac-kr.herokuapp.com/api' } = process.env
+
+export const callApi = async({url, method, token, body}) => {
+    try{ 
+        const options = { 
+            method: method ? method.toUpperCase() : 'GET',
+            headers: { 
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        };
+        if (token) options.headers['Authorixation'] = `Bearer ${token}`;
+
+
+        const response = await fetch (`${REACT_APP_API_URL}${url}`, options);
+        const data = await response.json();
+        if(data.error) { 
+            throw (data.error)
+        }
+    } catch (error) { 
+        console.error(error);
+    }
+    finally{}
+}
